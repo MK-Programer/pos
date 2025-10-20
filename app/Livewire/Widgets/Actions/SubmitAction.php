ccfd0bf9f64adc834ec122084a80ec7eec67c772
mustafakhaleedd <mustafakhaleedd@gmail.com>
2025-10-20 13:33:17 +0300

    Add reusable submit action component

---
+++ app/Livewire/Widgets/Actions/SubmitAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Widgets\Actions;
+
+use Livewire\Component;
+
+class SubmitAction extends Component
+{
+    public function render()
+    {
+        return view('livewire.widgets.actions.submit-action');
+    }
+}
